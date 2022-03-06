20dc088b0c814ea037ffa6daae22474272f9e3ba
sonia0409 <sonia.singh0409@gmail.com>
2022-03-05 19:50:54 -0500

    add sort the posts by asc or dsc

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const environment = 'dev';
 
 const app = express();
 const postsJSON = require('./data/data.json')
+const { sortBy } = require('./sortBy')
 
 //middleware setup
 app.use(morgan(environment));
@@ -31,8 +32,45 @@ app.get('/api/posts/:tags', (req, res) => {
         const arr = ((element.tags.filter(tag => input.includes(tag))))
         return arr.length > 0 ? true : false
     })
+
     res.json(postsFilteredByTag)
 
 })
+app.get('/api/posts/:tags/:key', (req, res) => {
+    const { tags, key } = req.params
+    console.log(tags, key)
+    const input = tags.split(',')
+    //console.log(input)
+
+    //console.log(postsJSON.posts)
+
+    const postsFilteredByTag = postsJSON.posts.filter(element => {
+        const arr = ((element.tags.filter(tag => input.includes(tag))))
+        return arr.length > 0 ? true : false
+    })
+
+    const sortedPosts =  postsFilteredByTag.sort(sortBy(key))
+    res.json(sortedPosts)
+
+})
+app.get('/api/posts/:tags/:key/:order', (req, res) => {
+    const { tags, key, order } = req.params
+    console.log(tags, key, order)
+    const input = tags.split(',')
+    //console.log(input)
+
+    //console.log(postsJSON.posts)
+
+    const postsFilteredByTag = postsJSON.posts.filter(element => {
+        const arr = ((element.tags.filter(tag => input.includes(tag))))
+        return arr.length > 0 ? true : false
+    })
+    console.log(typeof key, typeof order)
+    const sortedPosts =  postsFilteredByTag.sort(sortBy(key, order))
+    console.log(sortedPosts)
+    res.json(sortedPosts)
+
+})
+
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
